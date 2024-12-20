d656b54b88eda3f1af0be8ad875bf96377f57e41
Mahdi Bouaziz <mahdiibouaziz@gmail.com>
2024-12-20 15:01:54 +0100

    feat: discover more options

--- cmd/get/get.go
+++ cmd/get/get.go
@@ -6,9 +6,20 @@ import (
 )
 
 var GetCommand = &cobra.Command{
-	Use:   "get",
-	Short: "Get data based on an api",
-	Long:  "CLI subcommand to fetch data based on an API",
+	Use:        "get",
+	Short:      "Get data based on an api",
+	Long:       "CLI subcommand to fetch data based on an API",
+	Aliases:    []string{"fetch"},
+	SuggestFor: []string{"list"},
+	Example: `  # Get the first 10 users
+  cobra-playground get users
+  # Get the first 20 users 
+  cobra-playground get users -l 20
+  # Get users between 30 and 45
+  cobra-playground get users -b 30,45
+  # Get users wide format
+  cobra-playground get users -o wide
+	`,
 }
 
 func init() {
